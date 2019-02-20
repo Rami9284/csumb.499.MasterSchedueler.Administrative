383e855686b7de66208a2988480c760b4fdbd370
unknown <dochoa-aguila@csumb.edu>
2019-02-20 12:47:17 -0800

    init files

---
+++ src/test/java/com/csumb/Administrative/AdministrativeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csumb.Administrative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdministrativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
