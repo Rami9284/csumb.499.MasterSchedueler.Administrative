a6bf0b50b85daf7c66972a5e9347cc2967c8c0d5
rami9284 <judramirez@csumb.edu>
2019-02-21 12:16:43 -0800

    base for repositories

--- src/main/java/com/csumb/Administrative/repositotries/IStudentRepository.java
+++ src/main/java/com/csumb/Administrative/repositotries/IStudentRepository.java
@@ -2,10 +2,26 @@ package com.csumb.Administrative.repositotries;
 
 import com.csumb.Administrative.entities.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IStudentRepository extends MongoRepository<Student,String> {
 
+    @Query (value = "{'id':'?0'}")
+    
+    Student findId(String id);
+
+
+    @Query (value = "{'name':?0}")   
+
+    Student findName(String name); 
+
+    // delete query
+
+    // add query
+
+    // update query
+    
 }
 
--- src/main/java/com/csumb/Administrative/repositotries/ITeacherRepository.java
+++ src/main/java/com/csumb/Administrative/repositotries/ITeacherRepository.java
@@ -2,9 +2,24 @@ package com.csumb.Administrative.repositotries;
 
 import com.csumb.Administrative.entities.Teacher;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ITeacherRepository extends MongoRepository<Teacher,String> {
 
+    @Query (value = "{'id':'?0'}")
+    
+    Teacher findId(String id);
+
+
+    @Query (value = "{'name':?0}")   
+
+    Teacher findName(String name); 
+
+    // delete query
+
+    // add query
+
+    // update query
 }
