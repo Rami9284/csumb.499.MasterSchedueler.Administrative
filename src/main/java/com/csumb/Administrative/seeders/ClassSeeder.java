2c7de370aec9113c98ac5257d19d07b1f827daff
rami9284 <judramirez@csumb.edu>
2019-02-27 13:32:36 -0800

    Changed seeder

--- src/main/java/com/csumb/Administrative/seeders/ClassSeeder.java
+++ src/main/java/com/csumb/Administrative/seeders/ClassSeeder.java
@@ -19,21 +19,21 @@ public class ClassSeeder implements CommandLineRunner {
     public void run(String... args)throws  Exception{
         Class c1 = new Class("English", "Literature", "400");
        
-        // Class c2 = new Class("0000", "Math", "Geometry", "101", "216");
-        // Class c3 = new Class("0101", "Art", "Digital Art","201", "656");
-        // Class c4 = new Class("1234" , "Science", "Chemistry", "301", "356");
-        // Class c5 = new Class("3456", "English","AP English", "401", "760");
-        // Class c6 = new Class("2345", "History","Current Evevnts", "501", "627");
-        // Class c7 = new Class("4567", "Math", "Algebra 1", "102", "522");
-        // Class c8 = new Class("5678", "History", "World History", "502","627");
-        // Class c9 = new Class("6789", "English", "English1A", "402", "615");
-        // Class c10 = new Class("7890", "Art", "Graphic Design", "202", "656");
-        // Class c11 = new Class("8901", "Science", "Biology", "302", "876");
+        Class c2 = new Class( "Math", "Geometry", "101");
+        Class c3 = new Class("Art", "Digital Art","201");
+        Class c4 = new Class("Science", "Chemistry", "301");
+        Class c5 = new Class("English","AP English", "401");
+        Class c6 = new Class("History","Current Evevnts", "501");
+        Class c7 = new Class("Math", "Algebra 1", "102");
+        Class c8 = new Class("History", "World History", "502");
+        Class c9 = new Class("English", "English1A", "402");
+        Class c10 = new Class("Art", "Graphic Design", "202");
+        Class c11 = new Class("Science", "Biology", "302");
+        Class c12 = new Class("Social studies","World History Fast");
                 
 
         classRepository.deleteAll();
-        List<Class> classes = Arrays.asList(c1);//,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11);
-        //List<Class> classes = new ArrayList<>();
+        List<Class> classes = Arrays.asList(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12);
         classRepository.saveAll(classes);
     }
 
