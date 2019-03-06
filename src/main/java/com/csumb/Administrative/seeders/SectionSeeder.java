bdabc08e25b3cffc8d5c03fd8625c6f950167664
unknown <dochoa-aguila@csumb.edu>
2019-03-06 12:47:34 -0800

    seeder changes

--- src/main/java/com/csumb/Administrative/seeders/SectionSeeder.java
+++ src/main/java/com/csumb/Administrative/seeders/SectionSeeder.java
@@ -20,23 +20,22 @@ public class SectionSeeder implements CommandLineRunner {
     @Override
     public void run(String... args)throws  Exception{
 
-        Section s1 = new Section(new Class("English", "Literature", "400"),1);
-        Section s2 = new Section(new Class("English", "Literature", "400"),2);
-        Section s3 = new Section(new Class("Math", "Geometry", "101"),3);
-        Section s4 = new Section(new Class("Math", "Geometry", "101"),4);
-        Section s5 = new Section(new Class("Art", "Digital Art","201"),5);
-        Section s6 = new Section(new Class("Art", "Digital Art","201"),6);
-        Section s7 = new Section(new Class("Science", "Chemistry", "301"),7);
+        List<Class> classes = Arrays.asList(
+                new Class("English", "Literature", "400","123"),
+                new Class("Math", "Geometry", "101","321"),
+                new Class("Art", "Digital Art","201","435"),
+                new Class("Science", "Chemistry", "301","867"),
+                new Class("Science", "Chemistry", "301","654"),
+                new Class("English","AP English", "401","876"),
+                new Class("History","Current Events", "501","6752"),
+                new Class("English", "English1A", "402","5432"));
 
-        Section s8 = new Section(new Class("Science", "Chemistry", "301"),8);
-        Section s9 = new Section(new Class("English","AP English", "401"),9);
-        Section s10 = new Section(new Class("History","Current Events", "501"),10);
-        Section s11 = new Section(new Class("History","Current Events", "501"),11);
-        Section s12 = new Section(new Class("English", "English1A", "402"),12);
-        Section s13 = new Section(new Class("English", "English1A", "402"),13);
-
+       List<Section> sections = new ArrayList<>();
+       for(Class c : classes){
+           sections.add(new Section(c,1));
+           sections.add(new Section(c,2));
+       }
         sectionRepository.deleteAll();
-        List<Section> sections = Arrays.asList(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13);
         sectionRepository.saveAll(sections);
     }
 }
